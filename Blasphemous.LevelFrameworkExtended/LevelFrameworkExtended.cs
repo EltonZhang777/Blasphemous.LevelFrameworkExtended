3298c7ca9f9d5b61438925ae21a81dd410003c28
EltonZhang777 <280903158@qq.com>
2026-07-17 10:05:40 +0800

    Initial commit

---
+++ Blasphemous.LevelFrameworkExtended/LevelFrameworkExtended.cs
@@ -0,0 +1,13 @@
+﻿using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.LevelFrameworkExtended;
+
+public class LevelFrameworkExtended : BlasMod
+{
+    internal LevelFrameworkExtended() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        // Perform initialization here
+    }
+}
---
+++ Blasphemous.LevelFrameworkExtended/Main.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+namespace Blasphemous.LevelFrameworkExtended;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "3.0.1")]
+[BepInDependency("Blasphemous.Framework.Levels", "0.2.0")]
+internal class Main : BaseUnityPlugin
+{
+    public static LevelFrameworkExtended LevelFrameworkExtended { get; private set; }
+
+    private void Start()
+    {
+        LevelFrameworkExtended = new LevelFrameworkExtended();
+    }
+}
